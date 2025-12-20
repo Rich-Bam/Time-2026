fed21342f7d66cd574cd7f71b02906957a9dc811
Rich-Bam <richardbl184@gmail.com>
2025-12-20 23:50:38 +0100

    Fix build errors: add BugReports component and remove duplicate language keys

--- src/components/BugReports.tsx
+++ src/components/BugReports.tsx
@@ -1,1 +1,24 @@
+import React from "react";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
+interface BugReportsProps {
+  currentUser: any;
+}
+
+const BugReports = ({ currentUser }: BugReportsProps) => {
+  return (
+    <div className="space-y-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Bug Reports</CardTitle>
+          <CardDescription>View and manage bug reports</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Bug reports functionality will be implemented here.</p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default BugReports;
