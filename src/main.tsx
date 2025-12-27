6de25d8c32d65193882ae1e1854526b02601c8b8
Rich-Bam <richardbl184@gmail.com>
2025-12-27 19:09:50 +0100

    Fix: Remove serviceWorkerCleanup import from main.tsx (cleanup moved to index.html)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { initServiceWorkerCleanup } from './utils/serviceWorkerCleanup'
-
-// Cleanup old service workers and caches on startup (production only)
-initServiceWorkerCleanup();
 
 createRoot(document.getElementById("root")!).render(<App />);
